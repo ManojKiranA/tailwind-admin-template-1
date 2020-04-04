b7cbce3a972913ee0d4ec83c2ff668f640959e67
PK <pugazhwoo@gmail.com>
2020-04-04 14:52:25 +0530

    Dashboard card added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     extend: {
       colors: {
         "theme-color": "#486EDB",
+        "theme-hover-color": "#2e59d9",
+      },
+      boxShadow: {
+        default: "0 .15rem 1.75rem 0 rgba(58,59,69,.15)!important",
       },
     },
   },
