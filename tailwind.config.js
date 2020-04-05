b0c8b1febb1a2171f43bf71f83370ae67f4c87b5
PK <pugazhwoo@gmail.com>
2020-04-05 18:07:06 +0530

    Tables added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
       colors: {
         "theme-color": "#486EDB",
         "theme-hover-color": "#2e59d9",
+        "gray-50": "#f9fafb",
       },
       boxShadow: {
         default: "0 .15rem 1.75rem 0 rgba(58,59,69,.15)!important",
